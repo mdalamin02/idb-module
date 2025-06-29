73611151c9fb1acd827b5592368856c8c3086ff9
mmdalamin02 <contact.alamin02@gmail.com>
2025-06-29 18:39:35 +0600

    JSON

--- JavaScript/Class11(Window_Objec)/Fetch_Function/script.js
+++ JavaScript/Class11(Window_Objec)/Fetch_Function/script.js
@@ -1,3 +1,16 @@
-function loadComponent (id, file){
-    fetch(file).then(response => response)
+function loadComponent(id, file)
+{
+	fetch(file).then(response=>response.text()).then(data=>{
+
+		document.getElementById(id).innerHTML = data;
+	}).catch(error=>console.log("error loading:"+file,error));
 }
+
+window.onload = function()
+{
+	loadComponent("header","components/header.html");
+	loadComponent("navbar","components/navbar.html");
+	loadComponent("sidebar","components/sidebar.html");
+	loadComponent("search","components/search.html");
+	loadComponent("footer","components/footer.html");
+};
